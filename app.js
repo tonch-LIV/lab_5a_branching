b33addf62e1a54a2bbdccfdb076e790cd2485c53
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-02-13 23:36:08 -0800

    added multArray and passed, similar structure as previous work

--- app.js
+++ app.js
@@ -113,11 +113,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let total = multArr[0];
 
+  for (let i = 1; i < multArr.length; i++) {
+    total = multiply(total, multArr[i])[0];
+  }
+
+let production = 'The numbers ' + multArr + ' have a product of ' + total + '.';
+return [total, production];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
